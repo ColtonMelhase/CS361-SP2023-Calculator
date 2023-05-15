5c24c54d28b36592b851b9ad292043cc53cd1132
it-is-zane <=>
2023-05-15 12:27:49 -0700

    added comments, and adjusted the position of the input history

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "include/raylib.h"
+#include <iostream>
 #include <string>
 #include <tuple>
 #include <unordered_map>
@@ -7,8 +7,9 @@
 
 const int screenWidth = 800;
 const int screenHeight = 450;
-Rectangle textDisplay = { screenWidth/2 - 100, 180, 225, 50 };
+Rectangle textDisplay = {(float)screenWidth / 2 - 100, 180, 225, 50};
 
+// Gets keyboboard input and returns a string
 std::string GetCharsPressed() {
   std::string input;
   char character = GetCharPressed();
@@ -20,6 +21,8 @@ std::string GetCharsPressed() {
   return input;
 }
 
+// Returns the name of the pressed GUI button
+// or an empty string if no buttons were pressed
 std::string GetPressedButton() {
   // !!!todo!!!
   if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
@@ -29,13 +32,21 @@ std::string GetPressedButton() {
   return "";
 }
 
+// adds input to history
+// returns true when input is ready to be computed
 bool GetInput(std::vector<std::string> &history) {
+  // get input from gui buttons
   std::string output = GetPressedButton();
 
+  // if enter has been pressed on the gui or keyboard
+  // add new history entry and return true to alert the solver that
+  // the input is ready
   if (IsKeyPressed(KEY_ENTER) || output == "enter") {
     history.push_back("");
     return true;
-  } else if (IsKeyPressed(KEY_BACKSPACE) || output == "delete") {
+  }
+  // if backspace is pressed
+  else if (IsKeyPressed(KEY_BACKSPACE) || output == "delete") {
     if (history.back().size()) {
       history.back().pop_back();
     }
@@ -47,16 +58,18 @@ bool GetInput(std::vector<std::string> &history) {
   }
 }
 
+// Draws user input, errors, and solutions
 void DrawDisplay(std::vector<std::string> history, std::string answer = "") {
-  DrawText(history.back().c_str(), (int)textDisplay.x+5, (int)textDisplay.y + 8, 40, BLACK);
+  DrawText(history.back().c_str(), (int)textDisplay.x + 5,
+           (int)textDisplay.y + 8, 40, BLACK);
   for (int i = 0; i < history.size() - 1; i++) {
-    DrawText(history[i].c_str(), (int)textDisplay.x+5, 225 - 12 - 24 * (history.size() - i - 1),
-             24, GRAY);
+    DrawText(history[i].c_str(), (int)textDisplay.x + 5,
+             (int)textDisplay.y - 24 * (history.size() - i - 1), 24, GRAY);
   }
 }
 
 int main() {
-  
+
   InitWindow(screenWidth, screenHeight, "Calculator");
   SetTargetFPS(60);
 
@@ -66,17 +79,25 @@ int main() {
       {"tan", "cos"}, {"sin", "tan"}, {"blue"}};
   history.push_back("");
 
+  //
   while (!WindowShouldClose()) {
-
-    GetInput(history);
+    if (GetInput(history)) { // solve equation when true
+      // Solve second to last entry in history
+      // because last entry is current input
+      // Solve(history[history.size() - 2]);
+    }
 
     BeginDrawing();
     ClearBackground(WHITE);
 
-    DrawText("Calculator", (int)textDisplay.x, (int)textDisplay.y - 40, 24, BLACK);
-    DrawRectangleLines((int)textDisplay.x, (int)textDisplay.y, (int)textDisplay.width, (int)textDisplay.height, DARKGRAY);
-
     DrawDisplay(history);
+
+    DrawText("Calculator", (int)textDisplay.x, (int)textDisplay.y - 40, 24,
+             BLACK);
+
+    DrawRectangleLines((int)textDisplay.x, (int)textDisplay.y,
+                       (int)textDisplay.width, (int)textDisplay.height,
+                       DARKGRAY);
 
     EndDrawing();
   }
