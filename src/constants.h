f8bcaf8a38befefbf388d39b377b535a8479f36d
Colton Melhase <coltonmelhase12@gmail.com>
2023-05-17 15:20:16 -0700

    Added e and pi

--- src/constants.h
+++ src/constants.h
@@ -3,8 +3,13 @@
 
 #include "include/raylib.h"
 
+//UI constants
 const int screenWidth = 800;
 const int screenHeight = 450;
 const Rectangle textDisplay = {(float)screenWidth / 2 - 100, 180, 225, 50};
 
+
+//Math constants
+const double e = 2.71828183;
+const double pi = 3.14159265;
 #endif
