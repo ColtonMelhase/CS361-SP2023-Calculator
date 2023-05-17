81287da0cbce625887f8dd07bb271ebd947f419c
Colton Melhase <coltonmelhase12@gmail.com>
2023-05-17 15:15:00 -0700

    Added token class

--- src/solve.cpp
+++ src/solve.cpp
@@ -1,1 +1,37 @@
 #include "constants.h"
+#include <string>
+#include <deque>
+
+/*
+Token class
+To store necessary information for shunting yard algorithm
+*/
+class Token {
+    public:
+
+        enum class Type {
+            Number,
+            Constant,
+            Variable,
+            Operator,
+            Function,
+            LeftParenthesis,
+            RightParenThesis
+        };
+
+        Type type;
+        std::string str;
+        int precedence;
+        bool rightAssociative;
+
+        Token (Type t, std::string s, int prec = -1, bool ra = false) {
+            type = t;
+            str = s;
+            precedence = prec;
+            rightAssociative = ra;
+        }
+};
+
+std::deque<Token> expressionToTokens(std::string expr) {
+    std::deque<Token> tokens;
+}
