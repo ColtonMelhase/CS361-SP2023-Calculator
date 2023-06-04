4c6cf1dddfdefbd6ecd09ef302c04b6be6def2b9
Colton Melhase <coltonmelhase12@gmail.com>
2023-06-03 22:02:05 -0700

    Added default constructor and comments

--- src/storage.cpp
+++ src/storage.cpp
@@ -3,9 +3,16 @@
 
 class VarStorage {
     public:
-        void setVarValue(std::string key, double val);
-        double getVarValue(std::string key);
-        bool contains(std::string key);
+
+        //Default constructor which initializes with values of e and pi
+        VarStorage() {
+            vars["e"] = 2.71828183;
+            vars["pi"] = 3.14159265;
+        }
+        void setVarValue(std::string key, double val);  // set value of variable ex: x = 5 : setVarValue("x", 5)
+        double getVarValue(std::string key);            // get value of variable ex: getVarValue("x") returns 5
+        bool contains(std::string key);                 // returns true/false if the map already contains a variable name (not currently used, but may be needed if we want to warn
+                                                        // the user that they have redefined/overwritten an already defined variable for example)
     private:
         std::map<std::string, double> vars;
 };
@@ -37,5 +44,7 @@ int main() {
     //get variable value
     printf("\nThe value of variable 'a' is %f", store.getVarValue("a"));
     printf("\nThe value of variable 'b' is %f", store.getVarValue("b"));
+    printf("\nThe value of variable 'e' is %f", store.getVarValue("e"));
+    printf("\nThe value of variable 'pi' is %f", store.getVarValue("pi"));
 }
 */
