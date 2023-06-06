b25c120109e4aec66e8f3af2f837ea46fb76a02f
Colton Melhase <coltonmelhase12@gmail.com>
2023-06-05 19:50:17 -0700

    Edited prototype

--- src/calculus.cpp
+++ src/calculus.cpp
@@ -3,7 +3,7 @@
 class CalculusSolver {
     public:
         bool containsCalculus(std::string expr);
-        double computeCalculus(std::string &expr);
+        void replaceCalculus(std::string &expr);
         double integral(double a, double b, std::string expr, double d);
         double derivative(std::string expr, double post);
     private:
