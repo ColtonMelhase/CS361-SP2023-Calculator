ad54c049286851286f6e78cf46464688088d1b5e
Colton Melhase <coltonmelhase12@gmail.com>
2023-06-05 19:44:43 -0700

    Added function prototypes

--- src/calculus.cpp
+++ src/calculus.cpp
@@ -2,7 +2,10 @@
 
 class CalculusSolver {
     public:
-        bool containsCalculus(std::string);
+        bool containsCalculus(std::string expr);
+        double computeCalculus(std::string &expr);
+        double integral(double a, double b, std::string expr, double d);
+        double derivative(std::string expr, double post);
     private:
 
 };
