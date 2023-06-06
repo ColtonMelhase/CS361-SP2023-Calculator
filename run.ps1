35222eaf8c54e61843d4fcc73e32c0611bccc89b
Colton Melhase <coltonmelhase12@gmail.com>
2023-06-05 18:49:39 -0700

    Added -static flag

--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-g++ src/main.cpp src/button.cpp -o Calculator.exe -O1 -Wall -std=c++14 -Wno-missing-braces -I src/include/ -L src/lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ src/main.cpp src/button.cpp -o Calculator.exe -O1 -Wall -static -std=c++14 -Wno-missing-braces -I src/include/ -L src/lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
